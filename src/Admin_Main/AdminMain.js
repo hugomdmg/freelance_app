64bf32a17e99a59d540cd8e484042890e33ec640
hugomdmg <hugomdmg@gmail.com>
2024-12-17 21:18:13 +0000

    dates manage in admin

--- src/Admin_Main/AdminMain.js
+++ src/Admin_Main/AdminMain.js
@@ -57,7 +57,7 @@ const AdminMain = () => {
             </div>
             <div className="flex bg-[#d7e9e3] dark:bg-gray-900 min-h-screen ">
                 <div className="flex-1 bg-[#eaf1ef] dark:bg-gray-800 shadow-md rounded-lg p-6">
-                    <Dates dates={['01/01/2025', '20/01/2025']} />
+                    <Dates dates={selectedProject ? selectedProject.dates : []} />
                     <Chat user1={user} user2={selectedCostumer} />
                 </div>
             </div>
